2c4880a3b8551af625c18539a439aa9983db9d77
alejandro <alejandroweil13@gmail.com>
2021-01-19 16:50:47 -0500

    Started board.rb class for chess

--- Chess/board.rb
+++ Chess/board.rb
@@ -14,6 +14,28 @@ class Board
         @board = Array.new(8) { Array.new(8) }
     end
 
-    
+    def [](pos)
+        row, col = pos
+        self[row][col] 
+    end
+
+    def []=(pos)
+        row, col = pos
+        self[row][col] = pos
+    end
+
+    def move_piece(color, start_pos, end_pos)
+        if self[start_pos].empty? 
+            raise 'invalid start position'
+            
+            !valid_moves.include?(end_pos)   
+            self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
+
+    end
+
+    def move_piece!(color, start_pos, end_pos)
+
+    end
+
 
 end
