530dba20aaed7233c5a2b1c48d3b2174248f46cd
Christine <yang.christine.95@gmail.com>
2021-01-20 11:56:31 -0500

    Defined piece methods

--- Chess/Pieces/piece.rb
+++ Chess/Pieces/piece.rb
@@ -1,30 +1,32 @@
 class Piece
+    attr_reader :pos
 
     def initialize(color, board, pos)
         @board = Board.new
-        @color = color
+        @color = self.symbol # question
         @pos = pos # array
     end
 
     def to_s
-
+        "#{self.symbol}"
     end
 
     def empty?
-    
+        is_a?(NullPiece) 
     end
 
     def pos=(val)
-
+        @pos = val # will change current_pos to val_arr
     end
 
     def valid_moves # return an array
         
     end
 
-    def current_position
-
+    def symbol
     end
+
+    private
 
     def move_into_check?(end_pos)
 
